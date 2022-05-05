491abbc828b7da209bc91d472ea4961c21aeda9f
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-05-05 13:08:36 -0300

    fix

---
+++ Teste.Application/Dto/UsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Teste.Domain;
+
+namespace Teste.Application.Dto
+{
+    public class UsuarioDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O Campo {0} é obrigatório!")]
+        public string Name { get; set; }
+        public City City { get; set; }
+        public int CityId { get; set; }
+        public int Age { get; set; }
+        public string Birth { get; set; }
+    }
+}
---
+++ Teste.Application/Helpers/TesteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Teste.Application.Dto;
+using Teste.Domain;
+
+namespace Teste.Application.Helpers
+{
+    public class TesteProfile: Profile
+    {
+        public TesteProfile()
+        {
+            CreateMap<Usuario, UsuarioDto>();
+            CreateMap<UsuarioDto, Usuario>();
+        }
+    }
+}
